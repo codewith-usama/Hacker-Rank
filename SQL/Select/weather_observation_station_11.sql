363b1eddb4bdb3f0ca1f7216dce78457c0dc60df
Mohammad Usama <99090844+epiicuree@users.noreply.github.com>
2022-10-19 19:06:28 +0500

    Add files via upload

---
+++ SQL/Select/weather_observation_station_11.sql
@@ -0,0 +1,19 @@
+/*
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+The STATION table is described as follows:
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+*/
+
+SELECT DISTINCT(CITY)
+FROM STATION
+WHERE CITY REGEXP "^[^aeiou].+"
+    OR CITY REGEXP ".+[^aeiou]$";
---
+++ SQL/Select/weather_observation_station_8.sql
@@ -0,0 +1,18 @@
+/*
+Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+The STATION table is described as follows:
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+*/
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '^[AEIOU].+[aeiou]$'
+ORDER BY CITY;
